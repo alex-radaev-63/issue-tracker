237a8ccb97dcb48dad65b95717a1d89a68e08b8c
Alex Radaev <ar_info@alexradaev.design>
2025-10-09 14:52:29 -0400

    Styling active links - navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
 
   return (
-    <nav className="flex p-5 items-center gap-4 border-b border-black/15 mb-5">
+    <nav className="flex p-5 items-center gap-5 border-b border-black/15 mb-5">
       <Link href="/">
         <AiFillBug />
       </Link>
@@ -18,7 +24,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-600 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transitino-colors": true,
+            })}
           >
             {link.label}
           </Link>
