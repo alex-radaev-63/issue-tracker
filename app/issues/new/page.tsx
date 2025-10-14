70064781c04df765bf2ab9252cdecf5619070d88
Alex Radaev <ar_info@alexradaev.design>
2025-10-14 13:23:42 -0400

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,34 +2,65 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 
+import { useForm, Controller } from "react-hook-form";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
+import { useMemo } from "react";
+import axios from "axios";
+import { Options } from "easymde";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  const options = useMemo<Options>(
+    () => ({
+      spellChecker: false,
+      placeholder: "Issue Description",
+      hideIcons: ["quote", "preview", "side-by-side", "fullscreen", "guide"],
+      status: false,
+    }),
+    []
+  );
+
   return (
-    <div className="flex flex-col max-w-xl gap-4">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE
-        placeholder="Description"
-        options={{
-          spellChecker: false,
-          placeholder: "Issue Description",
-          hideIcons: [
-            "quote",
-            "preview",
-            "side-by-side",
-            "fullscreen",
-            "guide",
-          ],
-          status: false,
-        }}
+    <form
+      className="flex flex-col max-w-xl gap-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <SimpleMDE
+            placeholder="Description"
+            value={value}
+            onChange={onChange}
+            options={options}
+          />
+        )}
       />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
