b3a29ef83c887d94d3b65fef26a1f9a6aaf5f125
Alex Radaev <ar_info@alexradaev.design>
2025-10-15 11:25:33 -0400

    Add loading skeletons (create issue, issue details pages)

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Box, Card } from "@radix-ui/themes";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <Skeleton className="max-w-md" />
+      <div className="flex gap-3 my-2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </div>
+      <Card className="prose mt-4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
