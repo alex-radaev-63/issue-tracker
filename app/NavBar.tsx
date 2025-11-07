db889c4f788ff904293e2097afdf8d0daa55f8a2
Alex Radaev <ar_info@alexradaev.design>
2025-11-07 14:30:17 -0500

    Loading skeleton for login / logout link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,6 +7,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import { Skeleton } from "@/app/components";
 
 const NavBar = () => {
   return (
@@ -55,7 +56,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
   if (status === "unauthenticated")
     return (
       <Link href="/api/auth/signin" className="nav-link">
