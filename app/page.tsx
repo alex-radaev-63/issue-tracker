9790f6ec3e8841ca73f17476b4ca6c6e10da7778
Alex Radaev <ar_info@alexradaev.design>
2025-11-14 12:18:38 -0500

    Build "IssueSummary" component (Dashboard)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import { prisma } from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return <LatestIssue />;
+  const openCount = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressCount = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedCount = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return (
+    <IssueSummary
+      open={openCount}
+      inProgress={inProgressCount}
+      closed={closedCount}
+    />
+  );
 }
