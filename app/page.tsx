634c1937a0ee659263d3bc9f849a99eee2686804
Alex Radaev <ar_info@alexradaev.design>
2025-11-14 12:28:45 -0500

    Build the "Issue Chart" component (dashboard)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
+import { Flex } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
 
 export default async function Home({
   searchParams,
@@ -13,10 +15,18 @@ export default async function Home({
   });
   const closedCount = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <IssueSummary
-      open={openCount}
-      inProgress={inProgressCount}
-      closed={closedCount}
-    />
+    <Flex direction="column" gap="4">
+      <IssueSummary
+        open={openCount}
+        inProgress={inProgressCount}
+        closed={closedCount}
+      />
+      <LatestIssue />
+      <IssueChart
+        open={openCount}
+        inProgress={inProgressCount}
+        closed={closedCount}
+      />
+    </Flex>
   );
 }
